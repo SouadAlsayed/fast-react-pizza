e601fcbe9f8c108d1afbf19e19c13c4f22f22ec6
SouadAlsayed <souadalsayed1804@gmail.com>
2025-08-29 02:28:34 +0300

    fix(CreateOrder): adjust button position for better visibility

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -87,7 +87,10 @@ function CreateOrder() {
           </div>
 
           {!position.latitude && !position.longitude && (
-            <span className="absolute right-[3px] top-[3px] z-50 md:right-[5px] md:top-[5px]">
+            <span
+              className="absolute right-[3px] top-[35px] z-50 sm:right-[3px] sm:top-[3px]
+            md:right-[5px] md:top-[5px]"
+            >
               <Button
                 disabled={isLoadingAddress}
                 type="small"
